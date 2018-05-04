3a08df8e60f8a7b2bc1958adaf503787553aa900
litengbin <874214733@qq.com>
2018-05-05 06:45:21 +0800

    test six

---
+++ src/main/java/com/ltb/emall/test/TestAdUser.java
@@ -0,0 +1,40 @@
+package com.ltb.emall.test;
+
+import com.ltb.emall.util.RandomStringUtil;
+import org.junit.Test;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+/**
+ * ClassName: TestUser
+ * Description: user测试类
+ * User: litengbin
+ * Date: 2018/2/17 20:02
+ * Version: 1.0.0
+ */
+public class TestAdUser {
+    @Test
+    public static void main(String[] args) {
+        //准备用户测试数据：
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        try {
+            Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/emall_ssm?userUnicode=true&characterEncoding=utf8", "root", "123456");
+            Statement s = c.createStatement();
+            for (int i = 1; i <= 50; i++) {
+
+                String sqlFormat = "insert into adminuser values(null,'" + RandomStringUtil.getRandomCode(5,5) +"','"+ RandomStringUtil.getRandomCode(8,6)+ "')";
+//                String sql = String.format(sqlFormat, i, i);
+                s.execute(sqlFormat);
+            }
+            System.out.println("已经成功创建50条管理员测试数据");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
